c26e3efc75da07207de18b6ee3a8ed3714d8c79e
mayeDave <olorunmayedavid1234@gmail.com>
2026-02-18 07:57:21 +0100

    reviewed homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-import AuthPage from "./pages/Auth/AuthPage";
+// import AuthPage from "./pages/Auth/AuthPage";
 import DashboardPage from "./pages/Dashboard/DashboardPage";
 import SettingsPage from "./pages/Settings/SettingsPage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AuthPage />} />
+      <Route path="/" element={<DashboardPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
       <Route path="/settings" element={<SettingsPage />} />
       <Route path="*" element={<Navigate to="/" />} />
